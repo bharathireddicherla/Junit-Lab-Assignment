fedb30414f0b34450595750b8e8ae70c6c0abcb9
bharathireddicherla <68938765+bharathireddicherla@users.noreply.github.com>
2020-11-08 09:12:42 +0530

    Add files via upload

---
+++ BookDao.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+interface BookDao {
+	
+	public List<Book> getAllBooks();
+	public void addBook(Book book);
+	public Book updateBook(String iSBN, String bookName);
+	public void delBook(String iSBN);
+	
+
+}
